921e00fb28546c1f351b075612a0fc0284d2939f
Arqam Ahmad Siddiqui <arqamahmad365.au@gmail.com>
2023-04-09 23:04:48 +0530

    Practice Ques

---
+++ PracticeQuestion/src/main/kotlin/Main.kt
@@ -0,0 +1,65 @@
+// Write a program that takes an integer input from the user and prints "Even" if the input is an even number, and "Odd" if the input is an odd number.
+
+/*fun main() {
+    println("Enter the number: ")
+    val num = readlnOrNull()?.toIntOrNull()
+    if (num != null) {
+        if (num % 2 == 0) {
+            println("Even")
+        } else {
+            println("Odd")
+        }
+
+    }
+}*/
+
+// Write a program that takes a floating-point number as input from the user and prints "Positive" if the number is greater than zero, "Negative" if the number is less than zero, and "Zero" if the number is equal to zero.
+
+/*fun main() {
+    println("Enter the value: ")
+    val num = readln().toFloatOrNull()
+    if (num != null) {
+        if (num > 0) {
+            println("Positive")
+        } else if (num < 0) {
+            println("Negative")
+        } else {
+            println("Zero")
+        }
+    } else {
+        println("Invalid")
+    }
+}*/
+
+// Write a program that takes three integer inputs from the user and prints the largest number.
+
+/*fun main() {
+    println("Enter three numbers: ")
+    val num1 = readln().toIntOrNull()
+    val num2 = readln().toIntOrNull()
+    val num3 = readln().toIntOrNull()
+    if (num1 != null && num2 != null && num3 != null) {
+        if (num1 > num2) {
+            if (num1 > num3) {
+                println(num1)
+            }
+            else {
+                println(num2)
+            }
+        }
+        else {
+            if (num2 > num3) {
+                println(num2)
+            }
+            else {
+                println(num3)
+            }
+        }
+    }
+}*/
+
+// Write a program that takes an integer input from the user and prints "Leap Year" if the input is a leap year, and "Not a Leap Year" if the input is not a leap year. (A leap year is a year that is divisible by 4, but not divisible by 100 unless it is also divisible by 400).
+
+fun main() {
+    
+}
